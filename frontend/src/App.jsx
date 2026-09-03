e9156d1bef341da02e7de7fbd98806e7b06456cf
krishmitha2607 <dkrishmitha@gmail.com>
2026-09-03 17:16:16 +0530

    Remove simulate successful recovery button
    Removed button for simulating successful recovery.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1943,14 +1943,6 @@ function RecoveryModal({
         </button>
 
         <button
-          className="recover-success-button"
-          onClick={() => doAction("recover")}
-        >
-          <CheckCircle2 size={18} />
-          Simulate Successful Recovery
-        </button>
-
-        <button
           className="customer"
           onClick={customer}
         >
